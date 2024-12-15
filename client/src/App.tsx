37a23e4c8b75960a014c522306a09df251172034
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-12-15 13:28:09 +0530

    added loader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,29 @@
-import { lazy, Suspense } from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import ProtectRoute from "./components/auth/ProtectRoute"
-import { ThemeProvider } from "./context/UseContext"
-const Home = lazy(() => import("./pages/Home"))
-const Login = lazy(() => import("./pages/Login"))
-const Chat = lazy(() => import("./pages/Chat"))
-const Groups = lazy(() => import("./pages/Groups"))
-const NotFound = lazy(() => import("./pages/NotFound"))
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
+import { ThemeProvider } from "./context/UseContext";
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
-let user = true
+let user = true;
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 dark:border-indigo-400 border-solid"></div>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <ThemeProvider>
       <main className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
         <BrowserRouter>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<Loader />}>
             <Routes>
               <Route element={<ProtectRoute user={user} />}>
                 <Route path="/" element={<Home />} />
@@ -33,7 +41,7 @@ const App = () => {
         </BrowserRouter>
       </main>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
